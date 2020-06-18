a1e48c44436fd2d6625ef3e855d314eef550c934
Liu Jun <870395192@qq.com>
2020-06-18 16:01:29 +0800

    feat(git): 添加git hook
    添加githook 规范commit和支持change log

---
+++ .commitlintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', [
+            "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
+        ]],
+        'subject-full-stop': [0, 'never'],
+        'subject-case': [0, 'never']
+    }
+};
