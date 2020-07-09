efbca1dedac4de5871bb47f8985789fecfcbef6b
Liu Jun <870395192@qq.com>
2020-07-09 17:34:53 +0800

    chore(demo): 更新demo

--- packages/demo/src/vue-editor/views/editor/config/defaultItems.js
+++ packages/demo/src/vue-editor/views/editor/config/defaultItems.js
@@ -3,54 +3,54 @@
  */
 
 export default [
-    {
-        // 默认显示所有商品
-        name: 'CarouselImg',
-        value: {
-            imgList: [] // 设置长度不符合的 （错误格式的数据不做兼容）
-        }
-    },
-    {
-        // 默认显示所有商品
-        name: 'AllGoodsList',
-        value: {
-            show: true
-        }
-    },
-    {
-        name: 'Test',
-        value: {
-            title: 'My current tasks',
-            tasks: [
-                {
-                    title: 'My first task',
-                    details: `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-                    done: true
-                },
-                {
-                    title: 'My second task',
-                    details: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
-                    done: false
-                }
-            ]
-        }
-    },
-    {
-        name: 'TupleArray',
-        value: {
-            // address: ['32', 'St1', 'Avenue', 'NW', 'Additional Item'] // 设置长度不符合的 （错误格式的数据不做兼容）
-            address: [32, 'St1', 'Avenue', 'NW', 'Additional Item']
-        }
-    },
-    {
-        name: 'Text',
-        value: {
-            txt: '这里默认一个标题，嘿嘿嘿'
-        }
-    },
-    {
-        // 默认显示一个多图模块
-        name: 'MultipleImg3'
-    }
+    // {
+    //     // 默认显示所有商品
+    //     name: 'CarouselImg',
+    //     value: {
+    //         imgList: [] // 设置长度不符合的 （错误格式的数据不做兼容）
+    //     }
+    // },
+    // {
+    //     // 默认显示所有商品
+    //     name: 'AllGoodsList',
+    //     value: {
+    //         show: true
+    //     }
+    // },
+    // {
+    //     name: 'Test',
+    //     value: {
+    //         title: 'My current tasks',
+    //         tasks: [
+    //             {
+    //                 title: 'My first task',
+    //                 details: `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    //                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+    //                 done: true
+    //             },
+    //             {
+    //                 title: 'My second task',
+    //                 details: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur',
+    //                 done: false
+    //             }
+    //         ]
+    //     }
+    // },
+    // {
+    //     name: 'TupleArray',
+    //     value: {
+    //         // address: ['32', 'St1', 'Avenue', 'NW', 'Additional Item'] // 设置长度不符合的 （错误格式的数据不做兼容）
+    //         address: [32, 'St1', 'Avenue', 'NW', 'Additional Item']
+    //     }
+    // },
+    // {
+    //     name: 'Text',
+    //     value: {
+    //         txt: '这里默认一个标题，嘿嘿嘿'
+    //     }
+    // },
+    // {
+    //     // 默认显示一个多图模块
+    //     name: 'MultipleImg3'
+    // }
 ];
