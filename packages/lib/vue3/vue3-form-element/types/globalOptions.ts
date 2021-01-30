a12bc3df0682f55dfa32780cbc213bca32b583fc
Liu.Jun <870395192@qq.com>
2021-01-30 22:45:17 +0800

    chore(vue3-element): 更新 types d.ts

---
+++ packages/lib/vue3/vue3-form-element/types/globalOptions.ts
@@ -0,0 +1,13 @@
+declare namespace globalOptions {
+
+    /** WIDGET_MAP 配置 */
+    export const WIDGET_MAP:object
+
+    /** COMPONENT_MAP 配置 */
+    export const COMPONENT_MAP:object
+
+    /** ICONS_MAP 配置 */
+    export const ICONS_MAP:object
+}
+
+export default globalOptions;
