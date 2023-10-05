7a15c4237df396003e9c882215067a9e3f7ae3ad
Jino Liu <jino.liu@klook.com>
2023-10-05 21:50:57 +0800

    fix(lib): 补全类型

--- packages/lib/vue3/vue3-form-naive/types/index.d.ts
+++ packages/lib/vue3/vue3-form-naive/types/index.d.ts
@@ -8,9 +8,12 @@ import schemaValidate from './schemaValidate';
 import i18n from './i18n';
 import globalOptions from './globalOptions';
 
+type JsonSchemaFormAntdV4 = JsonSchemaForm
+
 export default JsonSchemaForm;
 
 export {
+    JsonSchemaFormAntdV4,
     globalOptions,
     getDefaultFormState,
     fieldProps,
